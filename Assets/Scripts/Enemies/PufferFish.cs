16f23b9d8bdd3d8e0733a77acdbab34ed3c3934e
yuilite <yuilite@gmail.com>
2025-01-26 01:59:07 +0800

    Enemy Update

--- Assets/Scripts/Enemies/PufferFish.cs
+++ Assets/Scripts/Enemies/PufferFish.cs
@@ -3,11 +3,30 @@ using UnityEngine;
 public class PufferFish : MonoBehaviour
 {
     public GameObject player;
+    public float destroyDistance;
     private float distance;
+    private Animator anim;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         distance = Vector3.Distance(player.transform.position, transform.position);
         Debug.Log(distance);
+
+        if (distance <= destroyDistance)
+        {
+            anim.SetTrigger("Alert");
+        }
+
+    }
+
+    public void DestroyFish()
+    {
+        Destroy(gameObject);
     }
 }
